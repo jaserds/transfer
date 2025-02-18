f692aa19e9c4391468064a44b6d71007aab08ad2
jaserds <52640ser@gmail.com>
2025-02-19 01:32:18 +0300

    Закончил работу 19.02.25.01.32

--- components/SectionTransfersComponent.tsx/TransfersContainerComponent.tsx
+++ components/SectionTransfersComponent.tsx/TransfersContainerComponent.tsx
@@ -9,7 +9,7 @@ const TransfersContainerComponent = () => {
         <section className='flex flex-col items-center mt-[120px] mb-[120px]'>
             <div className="px-[10px]">
                 <h2 className={`text-[#383F47] ${rubik.className} text-4xl font-semibold text-center mb-[70px]`} >Трансферы</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-[50px] justify-center">привет
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-[50px] justify-center">
                     <TransferComponent />
                     <TransferComponent />
                     <TransferComponent />
