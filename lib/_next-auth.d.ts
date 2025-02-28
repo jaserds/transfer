27e538a7c76ff3324dc355913c1b7fb19216e196
jaserds <52640ser@gmail.com>
2025-02-28 11:39:19 +0300

    работаю 28.02 число

--- lib/_next-auth.d.ts
+++ lib/_next-auth.d.ts
@@ -1,9 +1,7 @@
-import NextAuth from "next-auth"
-import { SessionEntity, UserEntity } from "./_domain/types";
+import { SessionEntity } from "./_domain/types";
 
 declare module "next-auth" {
     interface Session {
         user: SessionEntity["user"]
     }
-    interface User extends UserEntity { }
 }
